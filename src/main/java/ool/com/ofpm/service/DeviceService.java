ae8427c2baa11e966542e674073dee4975460b99
kurahashi <1131080355959@13G-U04602.nsl.ad.nec.co.jp>
2014-07-17 13:52:22 +0900

    Delete DeviceManagerService(Impl) PortService(Impl)
    to join
    DeviceService(Impl)

--- src/main/java/ool/com/ofpm/service/DeviceService.java
+++ src/main/java/ool/com/ofpm/service/DeviceService.java
@@ -2,17 +2,21 @@ package ool.com.ofpm.service;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Path("/device_mng")
 public interface DeviceService {
+	
+	// device service
 	@POST
 	@Consumes({ MediaType.APPLICATION_JSON })
 	@Produces({ MediaType.APPLICATION_JSON })
@@ -27,4 +31,27 @@ public interface DeviceService {
 	@Consumes({ MediaType.APPLICATION_JSON })
 	@Produces({ MediaType.APPLICATION_JSON })
 	public String updateDevice(@RequestBody String updateDeviceInfoJson);
+
+	// port service
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public String createPort(@RequestBody String newPortInfoJson);
+
+	@DELETE
+	@Consumes({ MediaType.APPLICATION_FORM_URLENCODED })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public String deletePort(@QueryParam("portName") String portName, @QueryParam("deviceName") String deviceName);
+
+	@PUT
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public String updatePort(@RequestBody String updatePortInfoJson);
+	
+	// other service
+	@GET
+	@Path("/connectedPort")
+	@Consumes({ MediaType.APPLICATION_FORM_URLENCODED })
+	@Produces({ MediaType.APPLICATION_JSON })
+	Response getConnectedPortInfo(@QueryParam("deviceName") String deviceName);
 }
--- src/main/java/ool/com/ofpm/service/DeviceServiceImpl.java
+++ src/main/java/ool/com/ofpm/service/DeviceServiceImpl.java
@@ -1,10 +1,15 @@
 package ool.com.ofpm.service;
 
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import ool.com.ofpm.business.DeviceBusiness;
 import ool.com.ofpm.business.DeviceBusinessImpl;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
@@ -21,7 +26,7 @@ public class DeviceServiceImpl implements DeviceService {
 
 	@Override
 	public String createDevice(String newDeviceInfoJson) {
-		String fname = "createDevice";
+		final String fname = "createDevice";
 		if (logger.isDebugEnabled()) {
 			logger.debug(String.format("%s(deviceInfoJson=%s) - start", fname, newDeviceInfoJson));
 		}
@@ -43,7 +48,7 @@ public class DeviceServiceImpl implements DeviceService {
 
 	@Override
 	public String deleteDevice(String deviceName) {
-		String fname = "deleteDevice";
+		final String fname = "deleteDevice";
 		if (logger.isDebugEnabled()) {
 			logger.debug(String.format("%s(deviceName=%s) - start", fname, deviceName));
 		}
@@ -65,7 +70,7 @@ public class DeviceServiceImpl implements DeviceService {
 
 	@Override
 	public String updateDevice(String updateDeviceInfoJson) {
-		String fname = "updateDevice";
+		final String fname = "updateDevice";
 		if (logger.isDebugEnabled()) {
 			logger.debug(String.format("%s(deviceInfoJson=%s) - start", fname, updateDeviceInfoJson));
 		}
@@ -83,5 +88,105 @@ public class DeviceServiceImpl implements DeviceService {
 			logger.debug(String.format("%s(ret=%s) - end", fname, resDeviceBiz));
 		}
 		return resDeviceBiz;
+	}
+
+	/* (non-Javadoc)
+	 * @see ool.com.ofpm.service.DeviceService#createPort(java.lang.String)
+	 */
+	@Override
+	public String createPort(@RequestBody String newPortInfoJson) {
+		final String fname = "createPort";
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format("%s(portInfoJson=%s) - start", fname, newPortInfoJson));
+		}
+
+		this.injector = Guice.createInjector(new AbstractModule() {
+			@Override
+			protected void configure() {
+				bind(DeviceBusiness.class).to(DeviceBusinessImpl.class);
+			}
+		});
+		DeviceServiceImpl main = this.injector.getInstance(DeviceServiceImpl.class);
+		String resDeviceBiz = main.deviceBiz.createPort(newPortInfoJson);
+
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format("%s(ret=%s) - end", fname, resDeviceBiz));
+		}
+		return resDeviceBiz;
+	}
+
+	/* (non-Javadoc)
+	 * @see ool.com.ofpm.service.DeviceService#deletePort(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public String deletePort(@QueryParam("portName") String portName, @QueryParam("deviceName") String deviceName) {
+		final String fname = "deletePort";
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format("%s(portName=%s, deviceName=%s) - start", fname, portName, deviceName));
+		}
+
+		this.injector = Guice.createInjector(new AbstractModule() {
+			@Override
+			protected void configure() {
+				bind(DeviceBusiness.class).to(DeviceBusinessImpl.class);
+			}
+		});
+		DeviceServiceImpl main = this.injector.getInstance(DeviceServiceImpl.class);
+		String resDeviceBiz = main.deviceBiz.deletePort(portName, deviceName);
+
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format("%s(ret=%s) - end", fname, resDeviceBiz));
+		}
+		return resDeviceBiz;
+	}
+
+	/* (non-Javadoc)
+	 * @see ool.com.ofpm.service.DeviceService#updatePort(java.lang.String)
+	 */
+	@Override
+	public String updatePort(@RequestBody String updatePortInfoJson) {
+		final String fname = "updatePort";
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format("%s(updatePortInfoJson=%s) - start", fname, updatePortInfoJson));
+		}
+
+		this.injector = Guice.createInjector(new AbstractModule() {
+			@Override
+			protected void configure() {
+				bind(DeviceBusiness.class).to(DeviceBusinessImpl.class);
+			}
+		});
+		DeviceServiceImpl main = this.injector.getInstance(DeviceServiceImpl.class);
+		String resDeviceBiz = main.deviceBiz.updatePort(updatePortInfoJson);
+
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format("%s(ret=%s) - end", fname, resDeviceBiz));
+		}
+		return resDeviceBiz;
+	}
+
+	/* (non-Javadoc)
+	 * @see ool.com.ofpm.service.DeviceService#getConnectedPortInfo(java.lang.String)
+	 */
+	@Override
+	public Response getConnectedPortInfo(@QueryParam("deviceName") String deviceName) {
+    	if (logger.isDebugEnabled()) {
+    		logger.debug(String.format("getConnectedPortInfo(deviceName=%s) - start ", deviceName));
+    	}
+
+        this.injector = Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+            	bind(DeviceBusiness.class).to(DeviceBusinessImpl.class);
+            }
+        });
+
+        DeviceServiceImpl main = injector.getInstance(DeviceServiceImpl.class);
+        String resDeviceBiz = main.deviceBiz.getConnectedPortInfo(deviceName);
+
+        if (logger.isDebugEnabled()) {
+    		logger.debug(String.format("getConnectedPortInfo(ret=%s) - end ", resDeviceBiz));
+    	}
+		return Response.ok(resDeviceBiz).type(MediaType.APPLICATION_JSON_TYPE).build();
 	}
 }
