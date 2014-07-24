93a9a4987bfef1969f466eee2b9c8dbf47446076
1131080355959 <1131080355959@13G-U04602.nsl.ad.nec.co.jp>
2014-07-24 16:33:59 +0900

    add interface logical_topology/setFlow

--- src/main/java/ool/com/ofpm/service/LogicalServiceImpl.java
+++ src/main/java/ool/com/ofpm/service/LogicalServiceImpl.java
@@ -8,11 +8,14 @@ import ool.com.ofpm.business.LogicalBusinessImpl;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
 
 @Component
 public class LogicalServiceImpl implements LogicalService {
@@ -64,5 +67,31 @@ public class LogicalServiceImpl implements LogicalService {
 			logger.debug(String.format("%s(ret=%s) - end", fname, resLogiBiz));
 		}
 		return Response.ok(resLogiBiz).type(MediaType.APPLICATION_JSON_TYPE).build();
+	}
+
+	/* (non-Javadoc)
+	 * @see ool.com.ofpm.service.LogicalService#setFlow(java.lang.String)
+	 */
+	@Override
+	public Response setFlow(String requestedData) {
+		String fname = "setFlow";
+		Client c = Client.create();
+		WebResource r = c.resource("http://172.16.1.85:28080/ofc/ryu/ctrl/test");
+		String html = r.post(String.class, "{}");
+		return Response.ok(html).type(MediaType.APPLICATION_JSON_TYPE).build();
+/*
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format("%s(requestedTopologyJson=%s) - start", fname, requestedData));
+		}
+		this.injector = Guice.createInjector(new AbstractModule() {
+			@Override
+			protected void configure() {
+				bind(LogicalBusiness.class).to(LogicalBusinessImpl.class);
+			}
+		});
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format("%s(ret=%s) - end", fname, resLogiBiz));
+		}
+*/
 	}
 }
